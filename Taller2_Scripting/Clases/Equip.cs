4216f359c19b33ea44c56b11491dba4ce1879f08
JuanJoseEspitia <juanjoseespitiag@gmail.com>
2022-04-02 15:03:52 -0500

    Commit_2

--- Taller2_Scripting/Clases/Equip.cs
+++ Taller2_Scripting/Clases/Equip.cs
@@ -6,13 +6,27 @@ using System.Threading.Tasks;
 
 namespace Taller2_Scripting.Clases
 {
-    internal class Equip
+    internal class Equip : Card
     {
-        private enum TargetAttribute {AP,RP,ALL }
-        uint EffectPoints;
-        private enum Affinity {Knight, Mage, Undead}
 
+        public enum TargetAttribute { AP, RP, ALL }
+        public enum Affinity { Knight, Mage, Undead }
+        private TargetAttribute targetAttribute;
+        private Affinity affinity;
 
+        uint effectPoints;
+        
+
+        public Character(string name, lrarety rarety, Affinity affinity,uint effectPoints, TargetAttribute targetAttribute) : base(name, rarety, costPoint)
+        {
+            EfectPoints = effectPoints;
+            TargetAttribute = targetAttribute;
+            Affinity = affinity;
+        }
+
+        public uint EffectPoints { get => effectPoints; set => effectPoints = value; }
+        public Affinity affinity { get => affinity; set => affinity = value; }
+        public TargetAttribute targetAttribute { get => targetAttribute; set => targetAttribute = value; }
 
     }
 }
